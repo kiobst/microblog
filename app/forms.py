a7c448f321ab84214df10b300e4252c2a8464c58
Kekoa <kekoaponolani@gmail.com>
2017-01-05 20:07:25 -0500

    Implemented Followers

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length
+from app.models import User
 
 
 class LoginForm(Form):
@@ -17,12 +18,12 @@ class EditForm(Form):
         self.original_nickname = original_nickname
 
     def validate(self):
-        if not Form.Validate(self):
+        if not Form.validate(self):
             return False
         if self.nickname.data == self.original_nickname:
             return True
-        user = User.query.filter_by(nickname-self.nickname.data).first()
-        if user != None:
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user is not None:
             self.nickname.errors.append('This nickname is already in use. Please choose another one.')
             return False
         return True
